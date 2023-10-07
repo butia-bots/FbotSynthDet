3fe53e5261fe4c1fe06b20dc34c6dc641d45e9c2
Igor Maurell <igormaurell@gmail.com>
2023-10-07 14:05:54 -0300

    adding new textures and randomizers

---
+++ SynthDet/Assets/Scripts/RandomizerTags/CameraPositionRandomizerTag.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Perception.Randomization.Randomizers;
+
+namespace SynthDet.RandomizerTags
+{
+    [AddComponentMenu("SynthDet/RandomizerTags/CameraPositionRandomizerTag")]
+    [RequireComponent(typeof(Camera))]
+    public class CameraPositionRandomizerTag : RandomizerTag { }
+}
+
